10b328770c172f7178ef99bf37edcbc099632adc
Camden Scholl <89877319+c-l-scholl@users.noreply.github.com>
2025-04-02 16:15:25 -0500

    sidebar text format

--- app/ui/search/sort-sidebar.tsx
+++ app/ui/search/sort-sidebar.tsx
@@ -1,59 +1,58 @@
 import Link from "next/link";
 
 const SortSidebar = () => {
-
 	return (
-			<nav >
-				<h3 className="font-semibold text-gray-600 text-sm">Sort by</h3>
-				<ul className="list-none">
-					<li className="py-1 text-sm">
-						<Link
-							href="/search"
-							target="_self"
-							className="hover:border-b hover:border-black"
-						>
-							Relevance
-						</Link>
-					</li>
-					<li className="py-1 text-sm">
-						<Link
-							href="/search"
-							target="_self"
-							className="hover:border-b hover:border-black"
-						>
-							Trending
-						</Link>
-					</li>
-					<li className="py-1 text-sm">
-						<Link
-							href="/search"
-							target="_self"
-							className="hover:border-b hover:border-black"
-						>
-							Latest Arrivals
-						</Link>
-					</li>
-					<li className="py-1 text-sm">
-						<Link
-							href="/search"
-							target="_self"
-							className="hover:border-b hover:border-black"
-						>
-							Price: High to low
-						</Link>
-					</li>
-					<li className="py-1 text-sm">
-						<Link
-							href="/search"
-							target="_self"
-							className="hover:border-b hover:border-black"
-						>
-							Price: Low to high
-						</Link>
-					</li>
-				</ul>
-			</nav>
-	)
-}
+		<nav>
+			<h3 className="font-semibold text-gray-600 text-sm">Sort by</h3>
+			<ul className="list-none">
+				<li className="py-1 text-sm">
+					<Link
+						href="/search"
+						target="_self"
+						className="hover:border-b hover:border-black"
+					>
+						Relevance
+					</Link>
+				</li>
+				<li className="py-1 text-sm">
+					<Link
+						href="/search"
+						target="_self"
+						className="hover:border-b hover:border-black"
+					>
+						Trending
+					</Link>
+				</li>
+				<li className="py-1 text-sm">
+					<Link
+						href="/search"
+						target="_self"
+						className="hover:border-b hover:border-black"
+					>
+						Latest Arrivals
+					</Link>
+				</li>
+				<li className="py-1 text-sm">
+					<Link
+						href="/search"
+						target="_self"
+						className="hover:border-b hover:border-black"
+					>
+						Price: High to low
+					</Link>
+				</li>
+				<li className="py-1 text-sm">
+					<Link
+						href="/search"
+						target="_self"
+						className="hover:border-b hover:border-black"
+					>
+						Price: Low to high
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+};
 
 export default SortSidebar;
