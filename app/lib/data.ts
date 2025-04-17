f1a583609b408b6a2561b308564641088191927e
Camden Scholl <89877319+c-l-scholl@users.noreply.github.com>
2025-04-17 09:47:38 -0500

    added error to supabase call

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,12 +1,13 @@
 import { createClient } from '@/app/supabase/server';
 import { Movie } from "./definitions";
+
 export const fetchMovies = async (limit: number = 10): Promise<Movie[]> => {
 	const supabase = await createClient();
 	
-		const { data: movies, error } = await supabase.from<any, Movie>('Movies').select().limit(limit);
-		if (error) {
-			console.error('Database error:', error);
-			throw new Error("Error with querying movies");
-		}
-		return movies;
+	const { data: movies, error } = await supabase.from<any, Movie>('Movies').select().limit(limit);
+	if (error) {
+		console.error('Database error:', error);
+		throw new Error("Error with querying movies");
+	}
+	return movies;
 }
