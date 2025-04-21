3ab93fe2989e87bbecfe110cf4a086580fd6aa8e
Camden Scholl <89877319+c-l-scholl@users.noreply.github.com>
2025-04-21 13:50:17 -0500

    finished checkout desktop

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -6,12 +6,14 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { CircleSlash2 } from "lucide-react";
+import CartItem from "@/app/ui/main/cart-item";
+import { Separator } from "@/components/ui/separator";
 
 const CheckoutPage = () => {
 	return (
 		<main className="max-w-8xl">
-			<div className="flex flex-col md:grid md:grid-cols-3 md:grid-rows-1">
-				<div className="flex flex-col px-16 py-8 h-full md:row-span-1 md:col-span-2 md:border-r md:border-neutral-50">
+			<div className="flex flex-row">
+				<div className="flex flex-col px-16 py-8 h-[100vh] md:w-1/2 md:border-r md:border-neutral-50">
 					<div className="mb-8">
 						<Link href="/">Home</Link>
 					</div>
@@ -38,26 +40,55 @@ const CheckoutPage = () => {
 							</div>
 							<div className="mb-8">
 								<h1 className="font-bold text-xl mb-2">Payment</h1>
-								<p className="text-sm text-gray-400">All transactions are not secure or encrypted.</p>
-								<div className="h-10 w-full bg-gray-500 rounded-xl">
+								<p className="text-sm text-gray-400 mb-4">
+									Transactions are not secure or encrypted.
+								</p>
+								<div className="flex flex-col justify-center items-center space-y-2 h-30 w-full bg-gray-700 opacity-40 rounded-lg">
 									<div>
 										<CircleSlash2 />
-
 									</div>
-									<div>
+									<p className="text-sm text-white">
 										This store cannot accept payments at this time.
-									</div>
+									</p>
 								</div>
 							</div>
 							<div className="flex justify-end items-center">
-								<Button type="submit" className="disabled:bg-blue-900 disabled:cursor-not-allowed" disabled>
+								<Button
+									type="submit"
+									className="disabled:bg-blue-900 disabled:cursor-not-allowed"
+									disabled
+								>
 									Pay Now
 								</Button>
 							</div>
 						</form>
 					</div>
 				</div>
-				<div className="flex md:row-span-1 md:col-span-1"></div>
+				<div className="flex flex-col px-16 py-8 md:w-1/2">
+					<div className="max-h-1/2 gap-4 mb-4 overflow-auto">
+						<div className="flex flex-col">
+							{/* items */}
+							<CartItem />
+							<CartItem />
+							<CartItem />
+							<CartItem />
+							<CartItem />
+						</div>
+					</div>
+					<Separator />
+					<div className="mt-4">
+						<div className="flex flex-col gap-2 mb-4 ">
+							<div className="flex flex-row justify-between items-center pb-1">
+								<div className="text-md font-semibold">Subtotal &#183; 3 items</div>
+								<div className="text-md font-semibold">$0.00</div>
+							</div>
+							<div className="flex flex-row justify-between items-center pb-1">
+								<div className="text-xl font-bold">Total</div>
+								<div className="text-xl font-bold">$0.00</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</main>
 	);
