71166f94fba2adc95cb69650903c8255a2b0a652
Tam Nguyen <tamnguyen01012001@gmail.com>
2025-04-24 16:25:26 -0500

    svms stuff

--- app/ui/fonts.ts
+++
@@ -1,7 +0,0 @@
-import { Inter } from "next/font/google";
-import { Lusitana } from "next/font/google";
-
-const inter = Inter({ subsets: ['latin']});
-const lusitana = Lusitana({ weight: ["400", "700"], subsets: ['latin'] });
-
-export { inter, lusitana };
