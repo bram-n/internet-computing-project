79c1e800509c29e3c8116329f9c7f7bf92fcb309
Camden Scholl <89877319+c-l-scholl@users.noreply.github.com>
2025-04-27 22:47:12 -0500

    made checkout page reactive

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -9,13 +9,19 @@ import { Button } from "@/components/ui/button";
 import { CircleSlash2, Loader2 } from "lucide-react";
 import CartItem from "@/app/ui/main/cart-item";
 import { Separator } from "@/components/ui/separator";
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const CheckoutPage = () => {
 	return (
-		<main className="max-w-8xl">
-			<div className="flex flex-row">
-				<div className="flex flex-col px-16 py-8 h-[100vh] md:w-1/2 md:border-r md:border-neutral-50">
-					<div className="mb-6">
+		<main className="max-w-xl lg:max-w-8xl flex flex-col justify-center mx-4 sm:mx-auto">
+			<div className="w-full flex flex-col justify-center lg:flex-row">
+				<div className="flex flex-col w-full lg:px-16 py-8 h-[100vh] lg:w-1/2 lg:border-r lg:border-neutral-50 order-last lg:order-first">
+					<div className="hidden mb-6 lg:flex">
 						<Link href="/">
 							<Image src="/logo.svg" alt="3AM Movies" width={40} height={40} />
 						</Link>
@@ -30,9 +36,14 @@ const CheckoutPage = () => {
 									placeholder="Email Address"
 									required
 								></Input>
-								<div className="flex items-top flex-row space-x-2">
-									<Checkbox />
-									<Label>Email me with news and offers</Label>
+								<div className="flex items-center flex-row space-x-2">
+									<Checkbox id="offers" />
+									<label
+										htmlFor="offers"
+										className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+									>
+										Email me with news and offers
+									</label>
 								</div>
 							</div>
 						</div>
@@ -55,13 +66,58 @@ const CheckoutPage = () => {
 								className="disabled:bg-blue-900 disabled:cursor-not-allowed"
 								disabled
 							>
-								<Loader2 className="animate-spin"/>
+								<Loader2 className="animate-spin" />
 								Pay Now
 							</Button>
 						</div>
 					</div>
 				</div>
-				<div className="flex flex-col px-16 py-8 md:w-1/2">
+				<div className="order-first my-6 flex lg:hidden">
+					<Link href="/">
+						<Image src="/logo.svg" alt="3AM Movies" width={40} height={40} />
+					</Link>
+				</div>
+				<div className="flex order-none justify-center lg:hidden">
+					<Accordion type="single" collapsible className="w-full border-y">
+						<AccordionItem value="item-1">
+							<AccordionTrigger>
+								<div className="w-full flex flex-row justify-between items-center">
+									<div>Order summary</div>
+									<div className="text-lg font-semibold">$0.00</div>
+								</div>
+							</AccordionTrigger>
+							<AccordionContent>
+								<div className="flex flex-col">
+									<div className="max-h-1/2 gap-4 mb-4 overflow-auto">
+										<div className="flex flex-col">
+											{/* items */}
+											<CartItem />
+											<CartItem />
+											<CartItem />
+											<CartItem />
+										</div>
+									</div>
+									<div className="mb-2">
+										<div className="flex flex-col gap-2">
+											<div className="flex flex-row justify-between items-center">
+												<div className="text-md font-semibold">
+													Subtotal &#183; 3 items
+												</div>
+												<div className="text-md font-semibold">$0.00</div>
+											</div>
+											<div className="flex flex-row justify-between items-center">
+												<div className="text-xl font-bold">Total</div>
+												<div className="text-xl font-bold">$0.00</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</AccordionContent>
+						</AccordionItem>
+					</Accordion>
+				</div>
+
+				<div className="hidden flex-col px-16 py-8 md:w-1/2 order-first lg:flex lg:order-last">
 					<div className="max-h-1/2 gap-4 mb-4 overflow-auto">
 						<div className="flex flex-col">
 							{/* items */}
