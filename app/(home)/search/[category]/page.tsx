a6e3eed975ef54d651c456bddee9e0bbe24506f1
Bram <bnutt@macalester.edu>
2025-05-07 17:39:43 -0500

    going back

--- app/(home)/search/[category]/page.tsx
+++ app/(home)/search/[category]/page.tsx
@@ -1,21 +1,9 @@
 import { fetchMoviesByGenre } from "@/lib/data";
 import MovieGrid from "@/app/ui/search/movie-grid";
 
-export type paramsType = Promise<{ query?: string;}>;
-
-export default async function Category( params : { params: paramsType }) {
-	const resolvedInnerParams = await params.params;
-	const categoryFromQuery = resolvedInnerParams.query;
-
-	if (!categoryFromQuery) {
-		return (
-			<main className="px-6 bg-black text-white min-h-screen">
-				<div>Error: Category query parameter is missing.</div>
-			</main>
-		);
-	}
-
-	const categoryName = decodeURIComponent(categoryFromQuery);
+export default async function Category({ params }: { params: { category: string } }) {
+	const { category } = await params;
+	const categoryName = decodeURIComponent(category);
 	const movies = await fetchMoviesByGenre(categoryName);
 	const formattedCategoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
 	
