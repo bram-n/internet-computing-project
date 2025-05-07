eaa2a17e315d75f687ccb506afcb199222daf983
c-l-scholl <cscholl@macalester.edu>
2025-05-07 16:36:10 -0500

    updated params in Category

--- app/(home)/search/[category]/page.tsx
+++ app/(home)/search/[category]/page.tsx
@@ -2,7 +2,7 @@ import { fetchMoviesByGenre } from "@/lib/data";
 import MovieGrid from "@/app/ui/search/movie-grid";
 
 export default async function Category({ params }: { params: { category: string } }) {
-	const { category } = await params;
+	const category = params.category;
 	const categoryName = decodeURIComponent(category);
 	const movies = await fetchMoviesByGenre(categoryName);
 	const formattedCategoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
