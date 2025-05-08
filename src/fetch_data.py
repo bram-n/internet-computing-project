09aea23c5476a0b825dea090a9faf36f2d237372
Bram <bnutt@macalester.edu>
2025-05-08 18:40:58 -0500

    tweak to fix user id fetch

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -11,7 +11,6 @@ def get_user_data(user_id):
     key: str = os.environ.get("NEXT_PUBLIC_SUPABASE_ANON_KEY")
     supabase: Client = create_client(url, key)
 
-    # user_id = '2e9ed51b-3e10-4f7d-8752-fd8f5bf80489' #to be replace with the id of current user
 
     response = (supabase.table("movie_likes")
                 .select('movie_id')
@@ -21,12 +20,7 @@ def get_user_data(user_id):
 
     liked_movies = [movie["movie_id"] for movie in response.data]
 
-    user_data = { "user_id": '2e9ed51b-3e10-4f7d-8752-fd8f5bf80489',
+    user_data = { "user_id": user_id,
                 "liked_movies": liked_movies}
 
-    # try:
-    #     with open("data/output.json", 'w') as f:
-    #         json.dump(user_data, f, indent=4)  # Use indent=4 for pretty formatting
-    # except Exception as e:
-    #         print(f"Error writing JSON to file: {e}")
     return user_data
