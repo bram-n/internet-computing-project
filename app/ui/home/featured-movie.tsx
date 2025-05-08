b22a15ba9bbebf4cfb54b4b045ffb4ab971e8003
c-l-scholl <cscholl@macalester.edu>
2025-05-08 16:17:04 -0500

    added links to featured items

--- app/ui/home/featured-movie.tsx
+++ app/ui/home/featured-movie.tsx
@@ -1,6 +1,7 @@
 import { getMoviePosterImage } from "@/lib/data";
 import { Movie } from "@/lib/definitions";
 import Image from "next/image";
+import Link from "next/link";
 
 interface FeaturedMovieCardProps {
 	movie: Movie;
@@ -10,17 +11,19 @@ const FeaturedMovieCard = async ({ movie }: FeaturedMovieCardProps) => {
 	const moviePoster = await getMoviePosterImage(movie.imdb_id);
 
 	return (
-		<div className="bg-neutral-900 rounded-lg overflow-hidden shadow transition-all duration-300 hover:shadow-xl hover:scale-102 mx-12 sm:mx-0 cursor-pointer">
-			<div className="overflow-hidden">
-				<Image
-					src={moviePoster.posterPath}
-					alt={movie.title}
-					width={400}
-					height={600}
-					className="w-full object-cover transition-transform duration-300 hover:scale-108"
-				/>
+		<Link href={`/search/movie/${movie.id}`}>
+			<div className="bg-neutral-900 rounded-lg overflow-hidden shadow transition-all duration-300 hover:shadow-xl hover:scale-102 mx-12 sm:mx-0 cursor-pointer">
+				<div className="overflow-hidden">
+					<Image
+						src={moviePoster.posterPath}
+						alt={movie.title}
+						width={400}
+						height={600}
+						className="w-full object-cover transition-transform duration-300 hover:scale-108"
+					/>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
